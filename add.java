15abb89b9097af405e157bc109f95205de77936d
Naga Sisir Srinivas V <vnsisirsrinivas@gmail.com>
2026-02-20 17:43:16 +0530

    Update 2 add.java

--- add.java
+++ add.java
@@ -1,11 +1,4 @@
-public class AddNumbers {
-    public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 10;
-        int sum = num1 + num2;
-        System.out.println("The sum is: " + sum);
-    }
-}
+
 public class Armstrong {
 
     public static void main(String[] args) {
